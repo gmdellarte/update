c3eb9be1a1c2a65adb0d3d39ff91d03a25b86a64
gmdellarte <gm.dellarte@gmail.com>
2016-11-06 13:28:55 +0100

    new folder

--- addon.py
+++ addon.py
@@ -6,6 +6,6 @@ addonname   = addon.getAddonInfo('name')
  
 line1 = "Hello World!"
 line2 = "We can write anything we want here"
-line3 = "Using Python"
+line3 = "Using Suka"
  
 xbmcgui.Dialog().ok(addonname, line1, line2, line3)
